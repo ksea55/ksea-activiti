a43eb2706b8dabe78d273fa60bfadab930fb0d2b
ksea55 <410223674@qq.com>
2018-01-06 21:41:30 +0800

    activiti 封装

--- ksea-activiti-project/src/main/java/org/ksea/activiti/utils/ActivitiUtils.java
+++ ksea-activiti-project/src/main/java/org/ksea/activiti/utils/ActivitiUtils.java
@@ -1,9 +1,15 @@
 package org.ksea.activiti.utils;
 
+
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.activiti.engine.impl.pvm.PvmTransition;
+import org.activiti.engine.impl.pvm.process.ActivityImpl;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 
 import javax.annotation.Resource;
 import java.io.File;
@@ -127,5 +133,151 @@ public class ActivitiUtils {
 
     }
 
+    /**
+     * 根据当前登录人，查询任务列表
+     *
+     * @param userId
+     * @return List<Task>
+     */
+    public List<Task> listTask(String userId) {
+
+        return processEngine.getTaskService()
+                .createTaskQuery()
+                .taskAssignee(userId)
+                .orderByTaskCreateTime()
+                .desc()
+                .list();
+    }
+
+
+    /**
+     * 根据taskId获取当前任务
+     *
+     * @param taskId
+     * @return Task
+     */
+    public Task getTaskById(String taskId) {
+
+        return processEngine.getTaskService()
+                .createTaskQuery()
+                .taskId(taskId)
+                .singleResult();
+    }
+
+
+    /**
+     * 根据taskId获取当前流程流程实例
+     *
+     * @param taskId
+     * @return ProcessInstance
+     */
+    public ProcessInstance getProcessInstanceByTaskId(String taskId) {
+        //根据taskId获取到当前流程实例Id
+        String processInstanceId = getTaskById(taskId).getProcessInstanceId();
+
+        //根据流程实例Id获取当前流程实例
+        return processEngine.getRuntimeService()
+                .createProcessInstanceQuery()
+                .processInstanceId(processInstanceId)
+                .singleResult();
+    }
+
+    /**
+     * 根据taskId获取当前流程定义
+     *
+     * @param taskId
+     * @return ProcessDefinition
+     */
+    public ProcessDefinition getProcessDefinitionByTaskId(String taskId) {
+
+        //根据taskId获取到当前流程定义Id
+        String processDefinitionId = getTaskById(taskId).getProcessDefinitionId();
+
+        //根据pdid获取当前流程定义
+        return processEngine.getRepositoryService()
+                .createProcessDefinitionQuery()
+                .processDefinitionId(processDefinitionId)
+                .singleResult();
+    }
+
+    /**
+     * 根据taskId获取流程定义实体
+     *
+     * @param taskId
+     * @return ProcessDefinitionEntity
+     */
+    private ProcessDefinitionEntity getProcessDefinitionEntityByTaskId(String taskId) {
+
+        String processDefinitionId = getTaskById(taskId).getProcessDefinitionId();
+
+        //根据流程定义Id-->pdid获取到流程定义实体 ProcessDefinitionEntity
+        ProcessDefinitionEntity processDefinitionEntity = (ProcessDefinitionEntity) processEngine.getRepositoryService()
+                .createProcessDefinitionQuery()
+                .processDefinitionId(processDefinitionId)
+                .singleResult();
+        return processDefinitionEntity;
+
+    }
+
+    /**
+     * 根据taskId获取当前流程活动节点 ActivityImpl
+     *
+     * @param taskId
+     * @return
+     */
+    public ActivityImpl getActivityImplByTaskId(String taskId) {
+
+        //获取当前流程实例的活动节点Id --> activityId
+        String activityId = getProcessInstanceByTaskId(taskId).getActivityId();
+
+        //获取当前流程定义实体 ProcessDefinitionEntity
+        ProcessDefinitionEntity processDefinitionEntity = getProcessDefinitionEntityByTaskId(taskId);
+
+        ActivityImpl activity = processDefinitionEntity.findActivity(activityId);
+
+        return activity;
+    }
+
+    /**
+     * 根据taskId获取当前活动任务节点上的所有SequenceFlow对象(PvmTransition),根据sequenceFlow的实体类获取到它的名称，动态生成每个任务节点的审批按钮描述信息
+     *
+     * @param taskId
+     * @return List<PvmTransition>
+     */
+
+    public List<PvmTransition> listSequenceFlowByTaskId(String taskId) {
+        //获取到当前正在执行的活动任务节点
+        ActivityImpl activity = getActivityImplByTaskId(taskId);
+
+        //获取到当前任务活动节点上的所有sequenceFlow
+        List<PvmTransition> pvmTransitions = activity.getOutgoingTransitions();
+
+        return pvmTransitions;
+
+    }
+
+
+    /**
+     * 根据taskId获取到外面与业务绑定的businesskey，从而根据该key关联其他
+     *
+     * @param taskId
+     * @return businesskey
+     */
+    public String businessKeyByTaskId(String taskId) {
+
+        return getProcessInstanceByTaskId(taskId).getBusinessKey();
+
+    }
+
+
+    public ProcessInstance finishTask(String taskId) {
+
+        //完成任务
+        processEngine.getTaskService().complete(taskId);
+
+        return getProcessInstanceByTaskId(taskId); //如果PI是null表明整个流程已经执行完毕，如果不为null表明流程正在执行中
+
+
+    }
 
 }
